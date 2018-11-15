9ed01bb5be5117f5130220daf0722f41eebdb003
刘国平 <guoping.liu@dianrong.com>
2018-11-15 17:10:15 +0800

    add hero-picker-view

--- src/dist/hero.js
+++ src/dist/hero.js
@@ -582,7 +582,7 @@
               this.parent.json.frame.h = yInt + hInt + '';
               this.parent.json.frame.w = xInt + wInt + '';
               frame = this.parent.json.frame;
-              this.parent.oon({ frame: frame });
+              this.parent.in({ frame: frame });
             }
           }
           if (this.json.contentSizeElementY) {
@@ -592,13 +592,13 @@
                 parseFloat(this.$.heroContent.style.height) +
                 '';
               frame = this.parent.json.frame;
-              this.parent.oon({ frame: frame });
+              this.parent.in({ frame: frame });
             }
           }
           if (this.heroLayoutListenners) {
             for (var i = 0; i < this.heroLayoutListenners.length; i++) {
               var o = this.heroLayoutListenners[i];
-              o.oon({ frame: o.json.frame, yOffset: o.json.yOffset });
+              o.in({ frame: o.json.frame, yOffset: o.json.yOffset });
             }
           }
         }
@@ -645,7 +645,7 @@
               this.parent.json.frame.h =
                 parseFloat(this.$.heroContent.style.top) + '';
               frame = this.parent.json.frame;
-              this.parent.oon({ frame: frame });
+              this.parent.in({ frame: frame });
             }
             this.$.heroContent.style.display = 'none';
           } else {
@@ -655,7 +655,7 @@
                 parseFloat(this.$.heroContent.style.height) +
                 '';
               frame = this.parent.json.frame;
-              this.parent.oon({ frame: frame });
+              this.parent.in({ frame: frame });
             }
             this.$.heroContent.style.display = 'block';
           }
@@ -721,22 +721,22 @@
             ')';
         }
         // if (json.gesture) {
-        // 	var gesture = json.gesture;
-        // 	var i, event;
-        // 	for (i = 0; i < gesture.length; i++) {
-        // 		var ges = gesture[i];
-        // 		if (ges.name === 'swip') {
-        // 			this.$.heroContent.addEventListener('touchmove',function(event){
-        // 				event = event || window.event;
-        // 				switch(event.type){
-        // 				case 'touchmove':
-        // 					event.touches[0].clientX;
-        // 				}
+        //  var gesture = json.gesture;
+        //  var i, event;
+        //  for (i = 0; i < gesture.length; i++) {
+        //    var ges = gesture[i];
+        //    if (ges.name === 'swip') {
+        //      this.$.heroContent.addEventListener('touchmove',function(event){
+        //        event = event || window.event;
+        //        switch(event.type){
+        //        case 'touchmove':
+        //          event.touches[0].clientX;
+        //        }
 
-        // 			}, false);
+        //      }, false);
 
-        // 		}
-        // 	}
+        //    }
+        //  }
         // }
         if (json.subViews) {
           while (this.$.heroContent.lastChild) {
@@ -2635,10 +2635,29 @@
   }
 
   class HeroImageView extends HeroElement {
+    constructor() {
+      super();
+
+      var _uploadUrl = '';
+      var _uploadName = '';
+      var _headers = '';
+      var _wrapper;
+      var _bigImg;
+      var _types = ['image/jpg', 'image/jpeg', 'image/png'];
+
+      this._uploadUrl = _uploadUrl;
+      this._uploadName = _uploadName;
+      this._headers = _headers;
+      this._wrapper = _wrapper;
+      this._bigImg = _bigImg;
+      this._types = _types;
+    }
     init() {
       this.shadowDom.querySelector('#heroContent').style.pointerEvents = 'none';
       this.$ = {
         img: this.shadowDom.querySelector('img'),
+        input: this.shadowDom.querySelector('input'),
+        upload: this.shadowDom.querySelector('#upload'),
       };
     }
 
@@ -2648,10 +2667,54 @@
         img {
           margin: 0px;padding: 0px;
           width: 100%;height: 100%;border: 0px;
-          pointer-events:none;
+          pointer-events:auto;
         }
-      </style>    
+        input {
+          display:none;
+        }
+        #upload{
+          margin: 0px;padding: 0px;
+          width: 100%;height: 100%;border: 0px;
+          pointer-events:auto;
+        }
+        label{
+          display:block;
+          height:100%;
+          width:100%;
+          background:#6f787b;
+          position:relative;
+        }
+        #upload-label::after {
+         content:'';
+         position:absolute;
+         top:0;
+         left:0;
+         bottom:0;
+         right:0;
+         margin:auto;
+         background: #000000;
+         height:75px;
+         width:10px;
+      }
+      #upload-label::before {
+        content:'';
+        position:absolute;
+        top:0;
+        left:0;
+        bottom:0;
+        right:0;
+        margin:auto;
+        background: #000000;
+        height:10px;
+        width:75px;
+     }
+                  
+      </style>  
       <img />
+      <div id='upload'>
+        <label for="file" id='upload-label'></label>
+        <input type="file" id="file" name="" style="display: none"/>
+      </div>
     `;
     }
 
@@ -2659,12 +2722,107 @@
       if (json.base64image || json.image) {
         this.$.img.src = json.base64image || json.image;
       }
-      if (json.uploadUrls) {
-        this.$.img.style.border = ' 1px dash #333';
-        var input = document.createElement('input');
-        input.type = 'image';
-        input.id = 'file_input';
-        this.$.heroContent.appendChild(input);
+      if (json.headers) {
+        this._headers = json.headers;
+      }
+      if (json.uploadName) {
+        this._uploadName = json.uploadName;
+      }
+      if (json.showBig) {
+        this.$.img.addEventListener('click', this.showBig.bind(this));
+        this._wrapper = document.createElement('div');
+        this._wrapper.style.height = '100%';
+        this._wrapper.style.width = '100%';
+        this._wrapper.style.background = 'rgba(0,0,0,0.8)';
+        this._wrapper.style.position = 'absolute';
+        this._wrapper.style.zIndex = '99998';
+        this._wrapper.style.transition = `all 0.5s`;
+        this._wrapper.style.opacity = 0;
+        this._wrapper.style.visibility = 'hidden';
+        this._wrapper.style.display = 'flex';
+        this._wrapper.style.justifyContent = 'center';
+        this._wrapper.style.alignItems = 'center';
+        this._wrapper.addEventListener('click', this.showSmall.bind(this));
+
+        this._bigImg = document.createElement('img');
+        this._bigImg.style.height = 'auto';
+        this._bigImg.style.width = '80%';
+        this._bigImg.src = json.base64image || json.image;
+        this._wrapper.appendChild(this._bigImg);
+      }
+      if (json.uploadUrl) {
+        let _script = document.createElement('script');
+        _script.src = './app/pickerviewbase/jquery-3.3.1.min.js';
+        document.body.appendChild(_script);
+        this.$.input.addEventListener('change', this.onChange.bind(this));
+        this.$.img.style.display = 'none';
+        this._uploadUrl = json.uploadUrl;
+      } else {
+        //fixd
+        this.$.upload.style.display = 'none';
+      }
+    }
+    showBig() {
+      document.body.appendChild(this._wrapper);
+      this._wrapper.style.visibility = 'visible';
+      this._wrapper.style.opacity = 1;
+    }
+    showSmall() {
+      this._wrapper.style.opacity = 0;
+      this._wrapper.style.visibility = 'hidden';
+    }
+    onChange() {
+      console.log(this._uploadUrl);
+      var formFile = new FormData();
+      var file = this.$.input.files[0];
+      if (file.size > 1 * 1024 * 1024) {
+        alert(`图片小于1mb${file.size}`);
+        return;
+      } else {
+        let _filetype = file.type;
+        for (let x in this._types) {
+          //判断格式是否匹配
+          if (_filetype == this._types[x]) {
+            //匹配
+            if (!formFile.get('upload')) {
+              formFile.append('upload', file);
+            } else {
+              formFile.set('upload', file);
+            }
+            $.ajax({
+              url: this._uploadUrl,
+              type: 'POST',
+              data: formFile,
+              async: true,
+              cache: false,
+              processData: false,
+              contentType: false,
+              success: res => {
+                console.log(res);
+                alert('上传成功');
+              },
+              error: res => {
+                console.log(res);
+                alert('上传失败');
+              },
+            });
+            var r = new FileReader();
+            let that = this;
+            r.onload = function() {
+              that.$.img.src = r.result;
+              that._bigImg.src = r.result;
+              that.$.img.style.display = 'block';
+            };
+            r.readAsDataURL(file);
+            break;
+          } else {
+            if (x == this._types.length - 1) {
+              //匹配失败
+              alert(`格式不对${file.type}`);
+              return;
+            }
+          }
+        }
       }
     }
   }
@@ -2699,6 +2857,11 @@
       }
       if (json.text) {
         this.$.input.value = json.text;
+        if (this._json.textFieldDidEditing) {
+          this._json.textFieldDidEditing.value = json.text;
+          this._json.textFieldDidEditing.name = this._json.name;
+          this.controller.on(this._json.textFieldDidEditing);
+        }
       }
 
       this.controller && this.controller.on(json.textFieldDidEditing);
@@ -3230,6 +3393,9 @@
         vertical-align: center;
         border-color: #fff;
         background-color: transparent;
+        border:none;
+        outline:none;
+        padding: 10px;
       }
       </style>
       <textarea id='text'></textarea>
@@ -3254,12 +3420,19 @@
 
       if (json.text) {
         this.$.textarea.value = json.text;
+        if (this._json.textFieldDidEditing) {
+          this._json.textFieldDidEditing.value = json.text;
+          this._json.textFieldDidEditing.name = this._json.name;
+          this.controller.on(this._json.textFieldDidEditing);
+        }
       }
     }
 
-    textChange(text) {
+    textChange(e) {
+      var text = e.target.value;
       if (this._json.textFieldDidEditing) {
         this._json.textFieldDidEditing.value = text;
+        this._json.textFieldDidEditing.name = this._json.name;
         this.controller.on(this._json.textFieldDidEditing);
       }
     }
@@ -5100,6 +5273,337 @@
     }
   }
 
+  class HeroPickerView extends HeroElement {
+    init() {
+      this.$ = {
+        picker: this.shadowDom.querySelector('#picker'),
+      };
+      var element = document.createElement('link');
+      element.rel = 'stylesheet';
+      element.href = '/dist/hero-picker-view/anypicker-font.css';
+      document.head.appendChild(element);
+
+      element = document.createElement('link');
+      element.rel = 'stylesheet';
+      element.href = '/dist/hero-picker-view/anypicker.css';
+      document.head.appendChild(element);
+
+      element = document.createElement('link');
+      element.rel = 'stylesheet';
+      element.href = '/dist/hero-picker-view/anypicker-ios.css';
+      document.head.appendChild(element);
+
+      element = document.createElement('script');
+      element.src = '/dist/hero-picker-view/jquery-1.11.1.min.js';
+      document.head.appendChild(element);
+      setTimeout(function() {
+        element = document.createElement('script');
+        element.src = '/dist/hero-picker-view/anypicker.js';
+        document.head.appendChild(element);
+      }, 500);
+    }
+
+    template(json) {
+      return `
+      <style>
+        #content{
+          display:none;
+        }
+      </style>
+      <div id='content'>
+        <input type="text" id='picker'></input>
+      </div>
+    `;
+    }
+
+    on(json) {
+      if (!json.method) {
+        return;
+      }
+      var soViewSections = {
+        header: [],
+        contentTop: [],
+        contentBottom: [],
+        footer: ['cancelButton', 'setButton'],
+      };
+      var $this = $(this.$.picker);
+      var ul = navigator.language.toLowerCase();
+      if (ul === 'zh-cn')
+        var io18n = {
+          setButton: '设置',
+          cancelButton: '取消',
+          shortMonths: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split(
+            '_'
+          ),
+          fullMonths: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split(
+            '_'
+          ),
+        };
+
+      if (json.selectAction) {
+        this.selectAction = json.selectAction;
+      }
+      if (json.type === 'date') {
+        $this.AnyPicker({
+          mode: 'datetime',
+          dateTimeFormat: 'MMMM d, yyyy',
+          theme: 'iOS',
+          layout: 'fixed',
+          viewSections: soViewSections,
+          i18n: io18n,
+        });
+      }
+      if (json.datas) {
+        var arr = (this.arr = json.datas);
+        if (Array.isArray(arr) && arr.length > 0) {
+          if (typeof arr[0] === 'string') {
+            this.arr.one = true;
+            // one dimension
+            var dataSource = [],
+              oArrComponents = [
+                {
+                  component: 0,
+                  name: '0',
+                  label: '0',
+                },
+              ],
+              oArrDataSource = [
+                {
+                  component: 0,
+                  data: dataSource,
+                },
+              ];
+
+            for (var i = 0, j = arr.length; i < j; i++) {
+              dataSource.push({
+                label: arr[i],
+                val: i,
+              });
+            }
+
+            $this.AnyPicker({
+              mode: 'select',
+              layout: 'fixed',
+              viewSections: soViewSections,
+              components: oArrComponents,
+              dataSource: oArrDataSource,
+              theme: 'iOS',
+              // onSetOutput:this.valueChange,
+              i18n: io18n,
+            });
+          } else if (typeof arr[0] === 'object') {
+            // two dimension
+            var arr0 = [];
+            for (var i = 0, j = arr.length; i < j; i++) {
+              arr0.push({
+                label: arr[i].title,
+                val: i,
+              });
+            }
+
+            function getData2(data1) {
+              var arr1 = [];
+              var tmpArr = arr[data1.val].rows;
+              for (var i = 0, j = tmpArr.length; i < j; i++) {
+                arr1.push({
+                  label: tmpArr[i],
+                  val: i,
+                });
+              }
+              return arr1;
+            }
+
+            function onChange(iComp, iRow, oSelectedValues, sSource) {
+              if (iComp === 0) {
+                var data1 = oSelectedValues.values[0];
+                this.setting.dataSource[1].data = getData2(data1);
+                this.reloadComponent(1, true);
+              }
+            }
+
+            var oArrComponents = [
+                {
+                  component: 0,
+                  name: '0',
+                  label: '0',
+                  width: '50%',
+                  textAlign: 'center',
+                },
+                {
+                  component: 1,
+                  name: '1',
+                  label: '1',
+                  width: '50%',
+                  textAlign: 'center',
+                },
+              ],
+              oArrDataSource = [
+                {
+                  component: 0,
+                  data: arr0,
+                },
+                {
+                  component: 1,
+                  data: getData2(arr0[0]),
+                },
+              ];
+            $this.AnyPicker({
+              mode: 'select',
+              layout: 'fixed',
+              viewSections: soViewSections,
+              components: oArrComponents,
+              dataSource: oArrDataSource,
+              onChange: onChange,
+              // onSetOutput:this.valueChange,
+              theme: 'iOS',
+              i18n: io18n,
+            });
+          }
+        }
+      }
+      if (json.method === 'show') {
+        // show the picker
+        $this.triggerHandler('focus');
+      }
+    }
+  }
+
+  class HeroLoading extends HeroElement {
+    init() {
+      this.$ = {
+        layer: this.shadowDom.querySelector('#layer'),
+        loading: this.shadowDom.querySelector('#loading'),
+      };
+    }
+
+    template() {
+      return `
+    <style type="text/css">
+    * {
+        width: 100%;
+        height: 100%;
+    }
+    #loading{
+        width: 100px;
+        height: 100px;
+        position: absolute;
+        left:50%;
+        top:50%;
+        -webkit-transform: translate(-50%,-50%);
+        transform: translate(-50%,-50%);
+        z-index: 99999;
+    }
+    #layer{
+        position: absolute;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+        z-index: 99998;
+        background-color:#000; 
+        filter: alpha(opacity=40); 
+        -moz-opacity: 0.4; 
+        opacity: 0.4; 
+    }
+    .loadEffect {
+        width: 100px;
+        height: 100px;
+        position: relative;
+    }
+    .loadEffect span{
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        background: #fff;
+        position: absolute;
+        -webkit-animation: load 1.04s ease infinite;
+    }
+    @-webkit-keyframes load{
+        0%{
+            opacity: 1;
+        }
+        100%{
+            opacity: 0.2;
+        }
+    }
+    .loadEffect span:nth-child(1) {
+        left: 0;
+        top: 50%;
+        margin-top:-8px;
+        -webkit-animation-delay:0.13s;
+    } 
+    .loadEffect span:nth-child(2) {
+        left: 14px;
+        top: 14px;
+        -webkit-animation-delay:0.26s;
+    }
+    .loadEffect span:nth-child(3) {
+        left: 50%;
+        top: 0;
+        margin-left: -8px;
+        -webkit-animation-delay:0.39s;
+    }
+    .loadEffect span:nth-child(4) {
+        top: 14px;
+        right:14px;
+        -webkit-animation-delay:0.52s;
+    }
+    .loadEffect span:nth-child(5) {
+        right: 0;
+        top: 50%;
+        margin-top:-8px;
+        -webkit-animation-delay:0.65s;
+    }
+    .loadEffect span:nth-child(6) {
+        right: 14px;
+        bottom:14px;
+        -webkit-animation-delay:0.78s;
+    }
+    .loadEffect span:nth-child(7) {
+        bottom: 0;
+        left: 50%;
+        margin-left: -8px;
+        -webkit-animation-delay:0.91s;
+    }
+    .loadEffect span:nth-child(8) {
+        bottom: 14px;
+        left: 14px;
+        -webkit-animation-delay:1.04s;
+    }
+    .loadText {
+        position: relative;
+        left: 15px;
+        top: 10px;
+        font-size: 15px;
+        color: #fff;
+    }
+
+    </style>
+    <div id='layer'>
+    <div id='loading'>
+    <div class="loadEffect">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    </div>
+    </div>
+    </div>
+    `;
+    }
+
+    on(json) {
+      if (typeof json.show === 'boolean') {
+        this.$.loading.active = json.show;
+        this.$.layer.style.visibility = json.show ? 'visible' : 'hidden';
+      }
+    }
+  }
+
   var components = [
     HeroElement,
     HeroButton,
@@ -5120,6 +5624,8 @@
     HeroConfirm,
     HeroDialog,
     HeroScrollView,
+    HeroLoading,
+    HeroPickerView,
   ];
 
   for (var i = 0, len = components.length; i < len; i++) {
